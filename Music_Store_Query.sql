6fd18f6f810f251e48303925f9d2eb80c3ca9f83
Akash Walaki <154545323+Akashwalaki@users.noreply.github.com>
2024-05-26 18:13:09 +0530

    Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -64,31 +64,30 @@ WHERE il.track_id IN(
 	JOIN genre g ON t.genre_id = g.genre_id
 	WHERE g.name LIKE 'Rock'
 )
-ORDER BY email;
-
+ORDER BY c.email;
 
 /* Method 2 */
 
-SELECT DISTINCT email AS Email,first_name AS FirstName, last_name AS LastName, genre.name AS Name
-FROM customer
-JOIN invoice ON invoice.customer_id = customer.customer_id
-JOIN invoiceline ON invoiceline.invoice_id = invoice.invoice_id
-JOIN track ON track.track_id = invoiceline.track_id
-JOIN genre ON genre.genre_id = track.genre_id
-WHERE genre.name LIKE 'Rock'
-ORDER BY email;
+SELECT DISTINCT c.email AS Email, c.first_name AS FirstName, c.last_name AS LastName, g.name AS Name
+FROM customer c
+JOIN invoice i ON i.customer_id = c.customer_id
+JOIN invoiceline il ON il.invoice_id = i.invoice_id
+JOIN track t ON t.track_id = il.track_id
+JOIN genre g ON g.genre_id = t.genre_id
+WHERE g.name LIKE 'Rock'
+ORDER BY c.email;
 
 
 /* Q2: Let's invite the artists who have written the most rock music in our dataset. 
 Write a query that returns the Artist name and total track count of the top 10 rock bands. */
 
-SELECT artist.artist_id, artist.name,COUNT(artist.artist_id) AS number_of_songs
-FROM track
-JOIN album ON album.album_id = track.album_id
-JOIN artist ON artist.artist_id = album.artist_id
-JOIN genre ON genre.genre_id = track.genre_id
-WHERE genre.name LIKE 'Rock'
-GROUP BY artist.artist_id
+SELECT at.artist_id, at.name,COUNT(at.artist_id) AS number_of_songs
+FROM track t
+JOIN album a ON a.album_id = t.album_id
+JOIN artist at ON at.artist_id = a.artist_id
+JOIN genre g ON g.genre_id = t.genre_id
+WHERE g.name LIKE 'Rock'
+GROUP BY at.artist_id
 ORDER BY number_of_songs DESC
 LIMIT 10;
 
@@ -117,11 +116,11 @@ so you need to use the InvoiceLine table to find out how many of each product wa
 for each artist. */
 
 WITH best_selling_artist AS (
-	SELECT artist.artist_id AS artist_id, artist.name AS artist_name, SUM(invoice_line.unit_price*invoice_line.quantity) AS total_sales
-	FROM invoice_line
-	JOIN track ON track.track_id = invoice_line.track_id
-	JOIN album ON album.album_id = track.album_id
-	JOIN artist ON artist.artist_id = album.artist_id
+	SELECT at.artist_id AS artist_id, at.name AS artist_name, SUM(il.unit_price*il.quantity) AS total_sales
+	FROM invoiceline il
+	JOIN track t ON t.track_id = il.track_id
+	JOIN album a ON a.album_id = t.album_id
+	JOIN artist at ON at.artist_id = a.artist_id
 	GROUP BY 1
 	ORDER BY 3 DESC
 	LIMIT 1
@@ -129,10 +128,10 @@ WITH best_selling_artist AS (
 SELECT c.customer_id, c.first_name, c.last_name, bsa.artist_name, SUM(il.unit_price*il.quantity) AS amount_spent
 FROM invoice i
 JOIN customer c ON c.customer_id = i.customer_id
-JOIN invoice_line il ON il.invoice_id = i.invoice_id
+JOIN invoiceline il ON il.invoice_id = i.invoice_id
 JOIN track t ON t.track_id = il.track_id
-JOIN album alb ON alb.album_id = t.album_id
-JOIN best_selling_artist bsa ON bsa.artist_id = alb.artist_id
+JOIN album a ON a.album_id = t.album_id
+JOIN best_selling_artist bsa ON bsa.artist_id = a.artist_id
 GROUP BY 1,2,3,4
 ORDER BY 5 DESC;
 
@@ -147,13 +146,13 @@ the maximum number of purchases is shared return all Genres. */
 
 WITH popular_genre AS 
 (
-    SELECT COUNT(invoice_line.quantity) AS purchases, customer.country, genre.name, genre.genre_id, 
-	ROW_NUMBER() OVER(PARTITION BY customer.country ORDER BY COUNT(invoice_line.quantity) DESC) AS RowNo 
-    FROM invoice_line 
-	JOIN invoice ON invoice.invoice_id = invoice_line.invoice_id
-	JOIN customer ON customer.customer_id = invoice.customer_id
-	JOIN track ON track.track_id = invoice_line.track_id
-	JOIN genre ON genre.genre_id = track.genre_id
+    SELECT COUNT(il.quantity) AS purchases, c.country, g.name, g.genre_id, 
+	ROW_NUMBER() OVER(PARTITION BY c.country ORDER BY COUNT(il.quantity) DESC) AS RowNo 
+        FROM invoiceline il 
+	JOIN invoice i ON i.invoice_id = il.invoice_id
+	JOIN customer c ON c.customer_id = invoice.customer_id
+	JOIN track t ON t.track_id = i.track_id
+	JOIN genre g ON g.genre_id = t.genre_id
 	GROUP BY 2,3,4
 	ORDER BY 2 ASC, 1 DESC
 )
@@ -164,12 +163,12 @@ SELECT * FROM popular_genre WHERE RowNo <= 1
 
 WITH RECURSIVE
 	sales_per_country AS(
-		SELECT COUNT(*) AS purchases_per_genre, customer.country, genre.name, genre.genre_id
-		FROM invoice_line
-		JOIN invoice ON invoice.invoice_id = invoice_line.invoice_id
-		JOIN customer ON customer.customer_id = invoice.customer_id
-		JOIN track ON track.track_id = invoice_line.track_id
-		JOIN genre ON genre.genre_id = track.genre_id
+		SELECT COUNT(*) AS purchases_per_genre, c.country, g.name, g.genre_id
+		FROM invoiceline il
+		JOIN invoice i ON i.invoice_id = il.invoice_id
+		JOIN customer c ON c.customer_id = il.customer_id
+		JOIN track t ON t.track_id = il.track_id
+		JOIN genre g ON g.genre_id = t.genre_id
 		GROUP BY 2,3,4
 		ORDER BY 2
 	),
@@ -179,9 +178,9 @@ WITH RECURSIVE
 		ORDER BY 2)
 
 SELECT sales_per_country.* 
-FROM sales_per_country
-JOIN max_genre_per_country ON sales_per_country.country = max_genre_per_country.country
-WHERE sales_per_country.purchases_per_genre = max_genre_per_country.max_genre_number;
+FROM sales_per_country spc
+JOIN max_genre_per_country mgpc ON spc.country = mgpc.country
+WHERE spc.purchases_per_genre = mgpc.max_genre_number;
 
 
 /* Q3: Write a query that determines the customer that has spent the most on music for each country. 
@@ -194,10 +193,10 @@ first find the most spent on music for each country and second filter the data f
 /* Method 1: using CTE */
 
 WITH Customter_with_country AS (
-		SELECT customer.customer_id,first_name,last_name,billing_country,SUM(total) AS total_spending,
+		SELECT c.customer_id, c.first_name, c.last_name, i.billing_country,SUM(i.total) AS total_spending,
 	    ROW_NUMBER() OVER(PARTITION BY billing_country ORDER BY SUM(total) DESC) AS RowNo 
-		FROM invoice
-		JOIN customer ON customer.customer_id = invoice.customer_id
+		FROM invoice i
+		JOIN customer c ON c.customer_id = i.customer_id
 		GROUP BY 1,2,3,4
 		ORDER BY 4 ASC,5 DESC)
 SELECT * FROM Customter_with_country WHERE RowNo <= 1
@@ -207,9 +206,9 @@ SELECT * FROM Customter_with_country WHERE RowNo <= 1
 
 WITH RECURSIVE 
 	customter_with_country AS (
-		SELECT customer.customer_id,first_name,last_name,billing_country,SUM(total) AS total_spending
-		FROM invoice
-		JOIN customer ON customer.customer_id = invoice.customer_id
+		SELECT c.customer_id, c.first_name, c.last_name, i.billing_country, SUM(i.total) AS total_spending
+		FROM invoice i
+		JOIN customer c ON c.customer_id = i.customer_id
 		GROUP BY 1,2,3,4
 		ORDER BY 2,3 DESC),
 
@@ -225,7 +224,5 @@ ON cc.billing_country = ms.billing_country
 WHERE cc.total_spending = ms.max_spending
 ORDER BY 1;
 
-
-/* source: www.youtube.com/@RishabhMishraOfficial */
 
 /* Thank You :) */
